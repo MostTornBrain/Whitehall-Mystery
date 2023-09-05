28b4a062393cf06e69eba11a88fb42cb4c9c1979
Brian Stormont <MostTornBrain@users.noreply.github.com>
2023-09-04 21:06:32 -0400

    Fiddling with new editor... nothing of substabce here.

--- whitehall_gui.py
+++ whitehall_gui.py
@@ -1,4 +1,4 @@
-'''
+"""
 MIT License
 
 Copyright (c) 2023 Brian Stormont
@@ -20,13 +20,11 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-'''
+"""
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GdkPixbuf, Pango, Gdk, GLib
 import whitehall as wh
-import re
-import random
 
 travel_images = ["nothing", "boat-100-white.png", "alley-100.png", "coach-100.png"]
         
